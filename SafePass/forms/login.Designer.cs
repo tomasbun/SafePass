c9bbe3d3f53b65c762cc09fe72f0d6ec2f066a86
tomasbun <tomas.bun@gmail.com>
2019-08-07 18:46:26 +0100

    registration form removed login form has dynamic panel

--- SafePass/forms/login.Designer.cs
+++ SafePass/forms/login.Designer.cs
@@ -30,82 +30,133 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login));
-            this.username_txtbox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.safepass_lbl = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.password_txtbox = new System.Windows.Forms.TextBox();
-            this.login_btn = new System.Windows.Forms.Button();
+            this.username_txtbox = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.repeat_password_txtbox = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.safepass_lbl = new System.Windows.Forms.Label();
-            this.register_lbl = new System.Windows.Forms.Label();
-            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.button2 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // username_txtbox
+            // button1
             // 
-            this.username_txtbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.username_txtbox.Font = new System.Drawing.Font("Agency FB", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.username_txtbox.ForeColor = System.Drawing.Color.DarkGray;
-            this.username_txtbox.Location = new System.Drawing.Point(40, 5);
-            this.username_txtbox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.username_txtbox.Multiline = true;
-            this.username_txtbox.Name = "username_txtbox";
-            this.username_txtbox.Size = new System.Drawing.Size(221, 35);
-            this.username_txtbox.TabIndex = 1;
-            this.username_txtbox.Text = "Username";
-            this.toolTip1.SetToolTip(this.username_txtbox, "Click register if You dont have username yet");
-            this.username_txtbox.Click += new System.EventHandler(this.username_txtbox_Click);
-            this.username_txtbox.Enter += new System.EventHandler(this.username_txtbox_Enter);
+            this.button1.BackColor = System.Drawing.Color.Teal;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(33, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(262, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // safepass_lbl
+            // 
+            this.safepass_lbl.AutoSize = true;
+            this.safepass_lbl.Font = new System.Drawing.Font("Bahnschrift Condensed", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.safepass_lbl.ForeColor = System.Drawing.Color.Teal;
+            this.safepass_lbl.Location = new System.Drawing.Point(81, 12);
+            this.safepass_lbl.Name = "safepass_lbl";
+            this.safepass_lbl.Size = new System.Drawing.Size(170, 58);
+            this.safepass_lbl.TabIndex = 7;
+            this.safepass_lbl.Text = "SafePass";
             // 
             // password_txtbox
             // 
             this.password_txtbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.password_txtbox.Font = new System.Drawing.Font("Agency FB", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.password_txtbox.ForeColor = System.Drawing.Color.DarkGray;
-            this.password_txtbox.Location = new System.Drawing.Point(40, 47);
+            this.password_txtbox.Location = new System.Drawing.Point(40, 48);
             this.password_txtbox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.password_txtbox.Multiline = true;
             this.password_txtbox.Name = "password_txtbox";
-            this.password_txtbox.Size = new System.Drawing.Size(221, 36);
-            this.password_txtbox.TabIndex = 2;
+            this.password_txtbox.Size = new System.Drawing.Size(213, 30);
+            this.password_txtbox.TabIndex = 1;
             this.password_txtbox.Text = "Password";
-            this.toolTip1.SetToolTip(this.password_txtbox, "Password must contain a number, special character and at leas one capital");
-            this.password_txtbox.Click += new System.EventHandler(this.password_txtbox_Click);
+            this.toolTip1.SetToolTip(this.password_txtbox, "Must contain at least 6 characters and a number");
             this.password_txtbox.Enter += new System.EventHandler(this.password_txtbox_Enter);
             // 
-            // login_btn
+            // username_txtbox
             // 
-            this.login_btn.BackColor = System.Drawing.Color.Teal;
-            this.login_btn.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.login_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.login_btn.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.login_btn.ForeColor = System.Drawing.Color.White;
-            this.login_btn.Location = new System.Drawing.Point(33, 214);
-            this.login_btn.Name = "login_btn";
-            this.login_btn.Size = new System.Drawing.Size(262, 42);
-            this.login_btn.TabIndex = 3;
-            this.login_btn.Text = "Login";
-            this.login_btn.UseVisualStyleBackColor = false;
-            this.login_btn.Click += new System.EventHandler(this.login_btn_Click);
+            this.username_txtbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.username_txtbox.Font = new System.Drawing.Font("Agency FB", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username_txtbox.ForeColor = System.Drawing.Color.DarkGray;
+            this.username_txtbox.Location = new System.Drawing.Point(40, 7);
+            this.username_txtbox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.username_txtbox.Multiline = true;
+            this.username_txtbox.Name = "username_txtbox";
+            this.username_txtbox.Size = new System.Drawing.Size(213, 30);
+            this.username_txtbox.TabIndex = 0;
+            this.username_txtbox.Text = "Username";
+            this.toolTip1.SetToolTip(this.username_txtbox, "Must be at least 5 characters long");
+            this.username_txtbox.Enter += new System.EventHandler(this.username_txtbox_Enter);
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.repeat_password_txtbox);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.pictureBox3);
             this.panel1.Controls.Add(this.pictureBox2);
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.password_txtbox);
+            this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.username_txtbox);
             this.panel1.ForeColor = System.Drawing.Color.Transparent;
-            this.panel1.Location = new System.Drawing.Point(33, 81);
+            this.panel1.Location = new System.Drawing.Point(33, 73);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(262, 84);
-            this.panel1.TabIndex = 6;
+            this.panel1.TabIndex = 12;
+            // 
+            // repeat_password_txtbox
+            // 
+            this.repeat_password_txtbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.repeat_password_txtbox.Font = new System.Drawing.Font("Agency FB", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.repeat_password_txtbox.ForeColor = System.Drawing.Color.DarkGray;
+            this.repeat_password_txtbox.Location = new System.Drawing.Point(40, 89);
+            this.repeat_password_txtbox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.repeat_password_txtbox.Multiline = true;
+            this.repeat_password_txtbox.Name = "repeat_password_txtbox";
+            this.repeat_password_txtbox.Size = new System.Drawing.Size(213, 30);
+            this.repeat_password_txtbox.TabIndex = 2;
+            this.repeat_password_txtbox.TabStop = false;
+            this.repeat_password_txtbox.Text = "Repeat password";
+            this.repeat_password_txtbox.Enter += new System.EventHandler(this.repeat_password_txtbox_Enter);
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Location = new System.Drawing.Point(8, 83);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(245, 1);
+            this.panel3.TabIndex = 12;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox3.ErrorImage = null;
+            this.pictureBox3.InitialImage = null;
+            this.pictureBox3.Location = new System.Drawing.Point(-1, 85);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(40, 37);
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
             // 
             // pictureBox2
             // 
@@ -125,7 +176,7 @@
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.pictureBox1.ErrorImage = null;
             this.pictureBox1.InitialImage = null;
-            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 1);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(40, 41);
             this.pictureBox1.TabIndex = 8;
@@ -134,57 +185,48 @@
             // panel2
             // 
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel2.Location = new System.Drawing.Point(8, 42);
+            this.panel2.Location = new System.Drawing.Point(8, 43);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(245, 1);
             this.panel2.TabIndex = 7;
             // 
-            // safepass_lbl
+            // button2
             // 
-            this.safepass_lbl.AutoSize = true;
-            this.safepass_lbl.Font = new System.Drawing.Font("Bahnschrift Condensed", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.safepass_lbl.ForeColor = System.Drawing.Color.Teal;
-            this.safepass_lbl.Location = new System.Drawing.Point(81, 12);
-            this.safepass_lbl.Name = "safepass_lbl";
-            this.safepass_lbl.Size = new System.Drawing.Size(170, 58);
-            this.safepass_lbl.TabIndex = 7;
-            this.safepass_lbl.Text = "SafePass";
-            // 
-            // register_lbl
-            // 
-            this.register_lbl.AutoSize = true;
-            this.register_lbl.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.register_lbl.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.register_lbl.ForeColor = System.Drawing.Color.DimGray;
-            this.register_lbl.Location = new System.Drawing.Point(131, 264);
-            this.register_lbl.Name = "register_lbl";
-            this.register_lbl.Size = new System.Drawing.Size(63, 23);
-            this.register_lbl.TabIndex = 4;
-            this.register_lbl.Text = "Register";
-            this.toolTip1.SetToolTip(this.register_lbl, "Create new profile");
-            this.register_lbl.Click += new System.EventHandler(this.register_lbl_Click);
-            this.register_lbl.MouseLeave += new System.EventHandler(this.register_lbl_MouseLeave);
-            this.register_lbl.MouseMove += new System.Windows.Forms.MouseEventHandler(this.register_lbl_MouseMove);
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.DimGray;
+            this.button2.Location = new System.Drawing.Point(33, 257);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(262, 42);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Register";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // login
             // 
-            this.AcceptButton = this.login_btn;
+            this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gainsboro;
             this.ClientSize = new System.Drawing.Size(334, 311);
-            this.Controls.Add(this.register_lbl);
-            this.Controls.Add(this.safepass_lbl);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.login_btn);
+            this.Controls.Add(this.safepass_lbl);
+            this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Palatino Linotype", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SafePass";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -193,17 +235,19 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox username_txtbox;
-        private System.Windows.Forms.TextBox password_txtbox;
-        private System.Windows.Forms.Button login_btn;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label safepass_lbl;
-        private System.Windows.Forms.Label register_lbl;
         private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox repeat_password_txtbox;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox password_txtbox;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox username_txtbox;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- SafePass/forms/login.cs
+++ SafePass/forms/login.cs
@@ -22,38 +22,30 @@ namespace SafePass
         public login()
         {
             InitializeComponent();
+            
             if (Directory.Exists(directory_path))
                 number_of_files = Directory.GetFiles(directory_path).Length;
             
             if ( (!Directory.Exists(directory_path)) || (number_of_files == 0) || (!File.Exists(file_path)) )
             {
+
                 Directory.CreateDirectory(directory_path);
                 File.Create(file_path);
-                MessageBox.Show("No profiles detected please register first! " + number_of_files );
-                this.Hide();
-                register register_form = new register();
-                register_form.ShowDialog();
-                this.Close();
+                MessageBox.Show("No profiles detected please register first! ");
+                panel1.Height = 125;
+                button1.Text = "Register";
+                button2.Text = "Login";
+
             }
             else
-                read_users();
-
-        }
-
-        private void username_txtbox_Click(object sender, EventArgs e)
-        {
-            if (username_txtbox.Text == "Username")
             {
-                username_txtbox.Text = "";
-                username_txtbox.ForeColor = Color.Black;
-            }    
-            
-            if (password_txtbox.Text == "")
-            {
-                password_txtbox.Text = "Password";
-                password_txtbox.ForeColor = Color.DarkGray;
+                read_users();   
             }
+            username_txtbox.Focus();
+
         }
+
+       
 
         private void username_txtbox_Enter(object sender, EventArgs e)
         {
@@ -68,21 +60,12 @@ namespace SafePass
                 password_txtbox.Text = "Password";
                 password_txtbox.ForeColor = Color.DarkGray;
             }
-        }
 
-        private void password_txtbox_Click(object sender, EventArgs e)
-        {
-            if (password_txtbox.Text == "Password")
+            if (repeat_password_txtbox.Text == "")
             {
-                password_txtbox.Text = "";
-                password_txtbox.ForeColor = Color.Black;
+                repeat_password_txtbox.Text = "Repeat password";
+                repeat_password_txtbox.ForeColor = Color.DarkGray;
             }
-
-            if (username_txtbox.Text == "")
-            {
-                username_txtbox.Text = "Username";
-                username_txtbox.ForeColor = Color.DarkGray;
-            }                   
         }
 
         private void password_txtbox_Enter(object sender, EventArgs e)
@@ -98,41 +81,105 @@ namespace SafePass
                 username_txtbox.Text = "Username";
                 username_txtbox.ForeColor = Color.DarkGray;
             }
-        }
-
-        private void register_lbl_MouseMove(object sender, MouseEventArgs e)
-        {
-            register_lbl.ForeColor = Color.Teal;
-        }
-
-        private void register_lbl_MouseLeave(object sender, EventArgs e)
-        {
-            register_lbl.ForeColor = Color.DimGray;
-        }
-
-        private void login_btn_Click(object sender, EventArgs e)
-        {
-            foreach (User u in all_users)
+            if (repeat_password_txtbox.Text == "")
             {
-                if ( (u.GetUsername() == username_txtbox.Text) && (u.GetPassword() == password_txtbox.Text) )
+                repeat_password_txtbox.Text = "Repeat password";
+                repeat_password_txtbox.ForeColor = Color.DarkGray;
+            }
+        }
+        
+        private void repeat_password_txtbox_Enter(object sender, EventArgs e)
+        {
+            if (repeat_password_txtbox.Text == "Repeat password")
+            {
+                repeat_password_txtbox.Text = "";
+                repeat_password_txtbox.ForeColor = Color.Black;
+            }
+
+            if (username_txtbox.Text == "")
+            {
+                username_txtbox.Text = "Username";
+                username_txtbox.ForeColor = Color.DarkGray;
+                            }
+            if (password_txtbox.Text == "")
+            {
+                password_txtbox.Text = "Password";
+                password_txtbox.ForeColor = Color.DarkGray;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Register")
+            {
+                if (username_txtbox.Text.Length < 5 )
                 {
+                    MessageBox.Show("Username has to be at least 5 characters long");
+                }
+                else if (username_txtbox.Text.Contains(' ') || password_txtbox.Text.Contains(' '))
+                {
+                    MessageBox.Show("No spaces allowed");
+                }
+                else if (password_txtbox.Text != repeat_password_txtbox.Text)
+                {
+                    MessageBox.Show("Passwords do not match");
+                }
+                else
+                {
+                    File.WriteAllText(file_path, username_txtbox.Text.Trim() + " " + password_txtbox.Text.Trim());
+                    MessageBox.Show("Registration successful!");
                     this.Hide();
                     Control control_form = new Control(username_txtbox.Text);
                     control_form.ShowDialog();
-                    this.Close();
+                    //this.Close();
                 }
             }
-            MessageBox.Show("Username or password incorect!");
+            else
+            {
+                foreach (User u in all_users)
+                {
+                    if ((u.GetUsername() == username_txtbox.Text) && (u.GetPassword() == password_txtbox.Text))
+                    {
+                        this.Hide();
+                        Control control_form = new Control(username_txtbox.Text);
+                        control_form.ShowDialog();
+                        this.Close();
+                    }
+                }
+                MessageBox.Show("Username or password incorect!");
+            }            
         }
 
-        private void register_lbl_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-            this.Hide();
-            register register_form = new register();
-            register_form.ShowDialog();
-            this.Close();
+            if (button2.Text == "Register")
+            {
+                panel1.Height = 125;
+                button1.Text = "Register";
+                button2.Text = "Login";
+                repeat_password_txtbox.TabStop = true;
+                username_txtbox.Focus();
+                username_txtbox.Text = "";
+                password_txtbox.Text = "Password";
+                password_txtbox.ForeColor = Color.DarkGray;
+                repeat_password_txtbox.Text = "Repeat password";
+                repeat_password_txtbox.ForeColor = Color.DarkGray;
+            }
+            else
+            {
+                panel1.Height = 84;
+                button1.Text = "Login";
+                button2.Text = "Register";
+                repeat_password_txtbox.TabStop = false;
+                username_txtbox.Focus();
+                username_txtbox.Text = "";
+                password_txtbox.Text = "Password";
+                password_txtbox.ForeColor = Color.DarkGray;
+                repeat_password_txtbox.Text = "Repeat password";
+                repeat_password_txtbox.ForeColor = Color.DarkGray;
+            }
         }
-        
+
         private void read_users()
         {
             String[] lines = File.ReadAllLines(file_path);
